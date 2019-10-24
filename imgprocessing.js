af9954f69d684da062753d7c36449d466bee6426
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2019-10-24 00:05:59 -0300

    Inverting colors from JS

--- imgprocessing.js
+++ imgprocessing.js
@@ -1,9 +1,26 @@
 var img = new Image();
-img.src = 'https://www.cosy.sbg.ac.at/~pmeerw/Watermarking/lena_color.gif';
-var canvas = document.getElementById('canvas');
-var ctx = canvas.getContext('2d');
+img.src = 'lena_color.gif';
 img.onload = function() {
-  ctx.drawImage(img, 0, 0);
-  img.style.display = 'none';
+  draw(this);
 };
 
+function draw(img) {
+  var canvas = document.getElementById('canvas');
+  var ctx = canvas.getContext('2d');
+  ctx.drawImage(img, 0, 0);
+  img.style.display = 'none';
+  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  var data = imageData.data;
+
+  var invert = function() {
+    for (var i = 0; i < data.length; i += 4) {
+      data[i]     = 255 - data[i];     // red
+      data[i + 1] = 255 - data[i + 1]; // green
+      data[i + 2] = 255 - data[i + 2]; // blue
+    }
+    ctx.putImageData(imageData, 0, 0);
+  };
+
+  var jsbtn = document.getElementById('jsbtn');
+  jsbtn.addEventListener('click', invert);
+}
