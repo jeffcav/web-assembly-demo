036bebfdb2910bef2ea730084b4078b7713a60fd
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2019-10-24 05:20:55 -0300

    Beautifying js code

--- imgprocessing.js
+++ imgprocessing.js
@@ -3,7 +3,6 @@ request.open('GET', 'imgfuncs.wasm');
 request.responseType = 'arraybuffer';
 request.send();
 request.onload = function() {
-  //enablew();
 };
 
 var img = new Image();
@@ -23,70 +22,47 @@ var importObject = {
             'emscripten_resize_heap': _emscripten_resize_heap}}
 
 
-function enablew() {
-  var bytes = request.response;
-
-  WebAssembly.instantiate(bytes, importObject).then(results => {
-      var winv = function() {
-        var canvas = document.getElementById('canvas');
-        var ctx = canvas.getContext('2d');
-        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-        var data = imageData.data;
-
-	var point = results.instance.exports.winvert(data);
-	var newdata = new Uint8ClampedArray(importObject.env.memory.buffer, point, canvas.width * canvas.height * 4);
-        var newimg = new ImageData(newdata, canvas.width, canvas.height)
-	ctx.putImageData(newimg, 0, 0);
-      }
-    var wasmbtn = document.getElementById('wasmbtn');
-    wasmbtn.addEventListener('click', winv);
-
-	  //results.instance.exports.winvert();
-  });
-}
 
 function draw(img) {
-  var canvas = document.getElementById('canvas');
-  var ctx = canvas.getContext('2d');
-  ctx.drawImage(img, 0, 0);
-  img.style.display = 'none';
-  var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-  var data = imageData.data;
+	// Get Canvas image
+	var canvas = document.getElementById('canvas');
+	var ctx = canvas.getContext('2d');
+	ctx.drawImage(img, 0, 0);
+	img.style.display = 'none';
+	var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	var data = imageData.data;
 
-  var invert = function() {
-    var  begin = performance.now();
-    for (var i = 0; i < data.length; i += 4) {
-      data[i]     = 255 - data[i];     // red
-      data[i + 1] = 255 - data[i + 1]; // green
-      data[i + 2] = 255 - data[i + 2]; // blue
-    }
-    ctx.putImageData(imageData, 0, 0);
-    var end = performance.now();
-    console.log(end-begin);
-  };
+	// Assing JS invert to a button
+	var invert = function() {
+		for (var i = 0; i < data.length; i += 4) {
+			data[i]     = 255 - data[i];     // red
+			data[i + 1] = 255 - data[i + 1]; // green
+			data[i + 2] = 255 - data[i + 2]; // blue
+		}
+		ctx.putImageData(imageData, 0, 0);
+  	};
 
-  var bytes = request.response;
+	var jsbtn = document.getElementById('jsbtn');
+	jsbtn.addEventListener('click', invert);
 
-  WebAssembly.instantiate(bytes, importObject).then(results => {
-	init_pointer = results.instance.exports.getdata();
-        var init_data = new Uint8ClampedArray(importObject.env.memory.buffer, init_pointer, canvas.width * canvas.height * 4);
-        var init_img = new ImageData(init_data, canvas.width, canvas.height);
-        init_img.data.set(data);
+	// Assign WASM Invert to a button
+	var bytes = request.response;
+	WebAssembly.instantiate(bytes, importObject).then(results => {
+		// Initialize WASM buffer with current Canvas content
+		init_pointer = results.instance.exports.getdata();
+		var init_data = new Uint8ClampedArray(importObject.env.memory.buffer, init_pointer, canvas.width * canvas.height * 4);
+		var init_img = new ImageData(init_data, canvas.width, canvas.height);
+		init_img.data.set(data);
 
-	var winvf = function() {
-		var begin = performance.now();
-		var p1 = results.instance.exports.winvert();
-		var d1 = new Uint8ClampedArray(importObject.env.memory.buffer, p1, canvas.width * canvas.height * 4);
-                var img1 = new ImageData(d1, canvas.width, canvas.height);
-		ctx.putImageData(img1, 0, 0);
-		var end = performance.now();
-		console.log(end-begin);
-	}
+		var winvf = function() {
+			var pointer = results.instance.exports.winvert();
+			var inv_data = new Uint8ClampedArray(importObject.env.memory.buffer, pointer, canvas.width * canvas.height * 4);
+        	        var inv_img = new ImageData(inv_data, canvas.width, canvas.height);
+			ctx.putImageData(inv_img, 0, 0);
+		}
 
-    var wasmbtn = document.getElementById('wasmbtn');
-    wasmbtn.addEventListener('click', winvf);
-  });
-
-  var jsbtn = document.getElementById('jsbtn');
-  jsbtn.addEventListener('click', invert);
+		var wasmbtn = document.getElementById('wasmbtn');
+		wasmbtn.addEventListener('click', winvf);
+	});
 }
+
