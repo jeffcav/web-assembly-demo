a4210f6b7a2db72fe537272f3014c30212acdee9
Jefferson Cavalcante <recife.rodrigo@gmail.com>
2019-10-24 04:42:16 -0300

    adding noise via wasm

--- imgfuncs.c
+++ imgfuncs.c
@@ -1,13 +1,77 @@
 #define HEIGHT 512
 #define WIDTH 512
+#include <stdlib.h> // for rand
 #include <emscripten.h>
 
-unsigned int data[WIDTH * HEIGHT];
+int data[WIDTH * HEIGHT];
 
-unsigned int* EMSCRIPTEN_KEEPALIVE winvert() {
-	int i, mask = 0xffffff00;
+int* EMSCRIPTEN_KEEPALIVE getdata() {
+	return &data[0];
+}
+
+int EMSCRIPTEN_KEEPALIVE setdata(int *buf) {
+	int i, j=0;
+	for (i = 0; i < WIDTH*HEIGHT; i++) {
+		data[i] = buf[i];
+		if (buf[i] > 0)
+			j = 1;
+	}
+	return j;
+}
+
+int * EMSCRIPTEN_KEEPALIVE paint(int seed) {
+	srand(seed);
+	int i;
+	/*
+ 	for (i = 0; i < HEIGHT*WIDTH; i++) {
+		data[i] = rand() % 0xffffffff;
+	}
+	*/
+	
+        for (i = 0; i < HEIGHT*WIDTH; i++) {
+                data[i] -= rand() % 0xff;
+        }
+        
+
+	return &data[0];
+}
+
+int* EMSCRIPTEN_KEEPALIVE winvert(int *src) {
+	int i, mask = 0xffffff00, val;
 	for (i = 0; i < HEIGHT*WIDTH; i++) {
-		data[i] = mask - data[i];
+		data[i] = src[i];
+		//data[i] = 0xabc123ab;
+
+		//val = (val & 0xff00ffff) | ((0x00ff0000-(val & 0x00ff0000)) & 0x00ff0000);
+		//data[i] = val;
+
+		//data[i] = data[i] + 0x1;
+		/*
+		unsigned char r,g,b,a;
+		a = (origin[i] & mask_red) >> 24;
+		b = (origin[i] & mask_green) >> 16;
+		g = (origin[i] & mask_blue) >> 8;
+		r = (origin[i] & mask_alpha);
+
+		r = 255 - r;
+		g = 255 - g;
+		b = 255 - b;
+
+		origin[i] = 0;
+
+		origin[i] |= a;
+		origin[i] <<= 8;
+
+		origin[i] |= b;
+		origin[i] <<= 8;
+
+		origin[i] |= g;
+		origin[i] <<= 8;
+
+		origin[i] |= r;
+
+		//origin[i] = mask - origin[i];
+		*/
 	}
 
 	return &data[0];
--- imgprocessing.js
+++ imgprocessing.js
@@ -3,15 +3,15 @@ request.open('GET', 'imgfuncs.wasm');
 request.responseType = 'arraybuffer';
 request.send();
 request.onload = function() {
-  enablew();
+  //enablew();
 };
-
 
 var img = new Image();
 img.src = 'lena_color.gif';
 img.onload = function() {
   draw(this);
 };
+
 
 var importObject = {
             'env': {
@@ -25,9 +25,18 @@ var importObject = {
 
 function enablew() {
   var bytes = request.response;
+
   WebAssembly.instantiate(bytes, importObject).then(results => {
       var winv = function() {
-	return results.instance.exports.winvert();
+        var canvas = document.getElementById('canvas');
+        var ctx = canvas.getContext('2d');
+        var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+        var data = imageData.data;
+
+	var point = results.instance.exports.winvert(data);
+	var newdata = new Uint8ClampedArray(importObject.env.memory.buffer, point, canvas.width * canvas.height * 4);
+        var newimg = new ImageData(newdata, canvas.width, canvas.height)
+	ctx.putImageData(newimg, 0, 0);
       }
     var wasmbtn = document.getElementById('wasmbtn');
     wasmbtn.addEventListener('click', winv);
@@ -52,6 +61,42 @@ function draw(img) {
     }
     ctx.putImageData(imageData, 0, 0);
   };
+
+  var bytes = request.response;
+
+  WebAssembly.instantiate(bytes, importObject).then(results => {
+    //const p0 = results.instance.exports.getdata();
+    //const d0 = new Uint8ClampedArray(importObject.env.memory.buffer, p0, canvas.width*canvas.height*4);
+    //console.log(results.instance.exports.setdata(data));
+
+    //d0 = data;
+    //const img0 = new ImageData(d0, canvas.width, canvas.height);
+
+    var winv = function() {
+	var imageData2 = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	 var data2 = imageData2.data;
+	//    var data3 = Uint32Array.from(data2);
+	//p1 = results.instance.exports.winvert(data3);
+
+      var mse = parseInt(Math.floor((Math.random() + 100) * 1000));
+      var p1 = results.instance.exports.paint(mse);
+      var d1 = new Uint8ClampedArray(importObject.env.memory.buffer, p1, canvas.width * canvas.height * 4);
+      var img1 = new ImageData(d1, canvas.width, canvas.height);
+
+
+	for (var i=0; i < data2.length; i+=4) {
+		img1.data[i] = data[i] + img1.data[i];
+		img1.data[i+1] = data[i+1] + img1.data[i];
+		img1.data[i+2] = data[i+2] + img1.data[i];
+	}
+
+
+      ctx.putImageData(img1, 0, 0);
+    }
+    
+    var wasmbtn = document.getElementById('wasmbtn');
+    wasmbtn.addEventListener('click', winv);
+  });
 
   var jsbtn = document.getElementById('jsbtn');
   jsbtn.addEventListener('click', invert);
